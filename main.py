0cbe63cf1dbc4b01a1d242ee655d57d8f727cb30
Joao-MCO <joaomco.oliveira@gmail.com>
2023-06-04 22:39:21 -0300

    Segundo Commit

--- main.py
+++ main.py
@@ -10,13 +10,3 @@ liga = res.json()['data']
 ovr = Liga.Liga(liga)
 man = Liga.Mandante(liga)
 vis = Liga.Visitante(liga)
-
-api += "times"
-res = requests.get(api)
-cl = res.json()['data']
-clubes = []
-for i in cl:
-    clubes.append([i['id'], i['cleanName']])
-
-df = pd.DataFrame(clubes)
-pd.DataFrame.to_json(df, "times.json")
