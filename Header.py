22dc0cda7e4c2149584e1316692d8c651df0b2a6
João Marcos Cardoso de Oliveira <117744361+Joao-MCO@users.noreply.github.com>
2023-06-27 03:16:39 -0300

    Rodando liso

--- Header.py
+++ Header.py
@@ -1,24 +1,24 @@
 import customtkinter as ctk
 
-class Header(ctk.CTkScrollableFrame):
+class Header(ctk.CTkFrame):
     def __init__(self, master, ovr, **kwargs):
-        super().__init__(master, **kwargs)
+        super().__init__(master,**kwargs)
 
-        titulo = ctk.CTkLabel(master=master, text=ovr.nome)
+        titulo = ctk.CTkLabel(master=master, text=ovr.nome.upper(), font=("Comfortaa", 24))
         titulo.pack(pady=12, padx=10)
 
         aux = "Id " + str(ovr.id)
-        id = ctk.CTkLabel(master=master, text=aux)
+        id = ctk.CTkLabel(master=master, text=aux.upper(), font=("Comfortaa", 18))
         id.pack(pady=1, padx=10)
 
         aux = str(ovr.pais)
-        pais = ctk.CTkLabel(master=master, text=aux)
+        pais = ctk.CTkLabel(master=master, text=aux.upper(), font=("Comfortaa", 18))
         pais.pack(pady=1, padx=10)
 
         aux = str(ovr.inicio) + " - " + str(ovr.fim)
-        ano = ctk.CTkLabel(master=master, text=aux)
+        ano = ctk.CTkLabel(master=master, text=aux.upper(), font=("Comfortaa", 18))
         ano.pack(pady=1, padx=10)
 
         aux = "Partidas: " + str(ovr.partidas)
-        partidas = ctk.CTkLabel(master=master, text=aux)
+        partidas = ctk.CTkLabel(master=master, text=aux.upper(), font=("Comfortaa", 18))
         partidas.pack(pady=1, padx=10)
